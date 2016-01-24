0dd265b83c02f2bb57f1f938233acdf4d49051b4
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-01-23 19:29:48 -0700

    New project for searching movies and practicing accessing data from APIs

---
+++ main.js
@@ -0,0 +1,108 @@
+var movieTitle = '';
+
+$(document).ready(function() {
+  $('.submit').on('click', function(event) {
+      event.preventDefault();
+      searchMovie();
+      $('#resultModal').show();
+    });
+
+  $('#resultModalOkay').on('click', function() {
+    $('#resultModal').hide();
+    resetForm();
+  });
+
+  $('.close').click(function() {
+    $('#resultModal').hide();
+    resetForm();
+  });
+
+
+  var searchMovie = function() {
+    var baseURL = 'http://www.omdbapi.com/?';
+    var title = $('.inputEmail-1').val() || $('.inputEmail-2').val();
+    var year = $('#inputText').val();
+    var movieSearch = ''
+    var yearSearch = 'y=' + year
+    var url = '';
+    if ($('#select').val() === 'One Movie') {
+      movieSearch = 't=' + title;
+    } else {
+      movieSearch = 's=' + title;
+    };
+    if (year.length > 0) {
+      url = baseURL + movieSearch + '&' + yearSearch;
+    } else {
+      url = baseURL + movieSearch;
+    };
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": url,
+      "method": "GET"
+    }
+
+    $.ajax(settings).done(function (response) {
+      $('#searchResults').text('');
+      if ($('#select').val() === 'One Movie') {
+        oneMovie(response);
+      } else {
+        multiMovie(response);
+        console.log(response);
+      };
+    });
+  };
+});
+
+var oneMovie = function(response) {
+      $('.modal-title').text('Search Results for ' + response.Title);
+      for (var key in response) {
+        if (key !== 'Poster')
+          $('#searchResults').append(key + ': ' + response[key] + '<br>');
+      };
+    };
+
+var multiMovie = function(response) {
+      $('.modal-title').text('Search Results');
+      var resultObj = {};
+      for (var i = 0; i < 5; i++) {
+        resultObj = response.Search[i];
+        console.log(resultObj);
+        for (var key in resultObj) {
+          if (key !== 'Poster' && key !== 'Type') {
+            $('#searchResults').append(key + ': ' + resultObj[key] + '<br>');
+          };
+        };
+        $('#searchResults').append('<br><br>');
+      };
+    };
+
+var updateMovie = function(title) {
+  var url = 'http://www.omdbapi.com/?t=' + title;
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": url,
+      "method": "GET"
+    }
+
+    $.ajax(settings).done(function (response) {
+      $('#searchResults').text('');
+      oneMovie(response);
+      console.log(response);
+    });
+};
+
+$('#searchResults').on('click', function() {
+  console.log($('#searchResults'));
+    var title = movieTitle;
+    console.log(title);
+    debugger;
+    updateMovie('Mulan');
+
+  });
+
+var resetForm = function() {
+  $('input').val('');
+};
